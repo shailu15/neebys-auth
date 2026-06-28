2524c33968abfc78b606a620a1533bb30027fd7c
Shailendar S <shailendarshai25@gmail.com>
2026-06-29 02:46:21 +0530

    Add profile endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,16 @@ from './oauth/oauth.module';
 import { AuthModule }
 from './auth/auth.module';
 
+import { AppController }
+from './app.controller';
+
 @Module({
   imports: [
     AuthModule,
     OauthModule,
+  ],
+  controllers: [
+    AppController,
   ],
 })
 export class AppModule {}
